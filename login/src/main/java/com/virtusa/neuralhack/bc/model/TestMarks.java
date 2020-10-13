d2105ac80d7820bbe3c46a2bfa7d340b8cf10a7f
vignesh suresh <53996565+srikrishnavignesh@users.noreply.github.com>
2020-10-13 11:50:24 +0530

    Add files via upload

--- login/src/main/java/com/virtusa/neuralhack/bc/model/TestMarks.java
+++ login/src/main/java/com/virtusa/neuralhack/bc/model/TestMarks.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
